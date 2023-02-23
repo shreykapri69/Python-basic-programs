c3c90a0dc522f76ba2f19ae8ede0929d898f151c
Shrey Kapri <124300646+shreykapri69@users.noreply.github.com>
2023-02-23 23:49:03 +0530

    Add files via upload

---
+++ To find ASCII value.py
@@ -0,0 +1,5 @@
+# Program to find the ASCII value
+value = input("Enter the single Character: ")
+print("The ASCII value is", ord(value))
+
+""" (ord) is a inbuilt function used to find the ASCII value"""
