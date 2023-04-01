fe7c2e0604340b189ca4a97456ed235632990d17
Shrey Kapri <124300646+shreykapri69@users.noreply.github.com>
2023-04-01 13:17:32 +0530

    Add files via upload

---
+++ To  use ceil and floor builtin function.py
@@ -0,0 +1,13 @@
+# ceil() and floor()
+
+import math
+ 
+a = 2.3
+ 
+#Value of the ceil of 2.3
+print ("The ceil of 2.3 is : ", end="")
+print (math.ceil(a))
+ 
+#Valur of the floor of 2.3
+print ("The floor of 2.3 is : ", end="")
+print (math.floor(a))
