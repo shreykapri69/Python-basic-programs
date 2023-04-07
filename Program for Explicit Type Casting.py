a68a0a823a315da6f3b6b238e69ea9b652957113
Shrey Kapri <124300646+shreykapri69@users.noreply.github.com>
2023-04-07 11:14:35 +0530

    Add files via upload

---
+++ Program for Explicit Type Casting.py
@@ -0,0 +1,22 @@
+#Type Cating in Python
+
+#From Float to Integer
+a= 2.5
+b= int(a)
+print("From Float to Integer, the value= : ", b)
+
+#From Integer to String
+x= 6
+y= str(x)
+print("\nFrom Integer to String, the value= :", y)
+
+#From String to Integer
+m= "10"
+n= int(m)
+print("\nFrom String to Integer, the value= :", n)
+
+#From String to Float
+g= "20.6"
+h= float(g)
+print("\nFrom String to Float, the value= :", g)
+
