de729eadaf57e62192e5635bf100125aa146ecca
Shrey Kapri <124300646+shreykapri69@users.noreply.github.com>
2023-04-08 16:26:49 +0530

    Add files via upload

---
+++ Program for Comaparison Operators.py
@@ -0,0 +1,19 @@
+#Comparison Operators
+"""
+x == y               # True if ->  x is equal to y
+x != y               # True if ->  x is not equal to y
+x > y                # True if ->  x is greater than y
+x < y                # True if ->  x is less than y
+x >= y               # True if ->  x is greater than or equal to y
+x <= y               # True if ->  x is less than or equal to y
+"""
+
+x= 10
+y= 20
+
+print(x==y   )
+print(x != y )               
+print(x > y  )               
+print(x < y  )              
+print(x >= y )             
+print(x <= y ) 
