dea4063aaf6fefa8166c19aebbf69470bd526b13
Shrey Kapri <124300646+shreykapri69@users.noreply.github.com>
2023-04-09 09:22:07 +0530

    Add files via upload

---
+++ File Handling,  r,  To open a file in reading mode.py
@@ -0,0 +1,8 @@
+#File with a name Document, will open in the reading mode only.
+file = open('Document.txt', 'r')
+
+#r is used to open a file in reading mode only.
+
+#To print each and every line present in the file
+for i in file:
+	print (i)
