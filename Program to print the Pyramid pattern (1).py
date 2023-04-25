a4d2c4c83ef8deda8c3f4d604d36c1493967eda0
Shrey Kapri <124300646+shreykapri69@users.noreply.github.com>
2023-04-25 22:42:28 +0530

    Add files via upload

---
+++ Program to print the Pyramid pattern (1).py
@@ -0,0 +1,14 @@
+#To print the given pattern
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+"""
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("* ", end="")
+    print("\n")
