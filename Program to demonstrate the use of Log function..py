b81c9c677a1d21db98bf8ba1584c7f5022d39a1d
Shrey Kapri <124300646+shreykapri69@users.noreply.github.com>
2023-05-08 21:53:41 +0530

    Add files via upload

---
+++ Program to demonstrate the use of Log function..py
@@ -0,0 +1,13 @@
+# Python cto demonstrate the working of logarithm
+ 
+import math
+ 
+ 
+print ("The value of log 2 with base 3 is : ", end="")
+print (math.log(2,3))
+ 
+print ("The value of log2 of 16 is : ", end="")
+print (math.log2(16))
+    
+print ("The value of log10 of 10000 is : ", end="")
+print (math.log10(10000))
