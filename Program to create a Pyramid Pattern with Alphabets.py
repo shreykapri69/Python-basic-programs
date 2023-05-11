e1640dbfa4ef4965a38ba252658f934f1fd66a24
Shrey Kapri <124300646+shreykapri69@users.noreply.github.com>
2023-05-11 22:34:09 +0530

    Add files via upload

---
+++ Program to create a Pyramid Pattern with Alphabets.py
@@ -0,0 +1,25 @@
+#Program to demonstrate star pattern with alphabets.
+"""A 
+B B 
+C C C 
+D D D D 
+E E E E E """
+
+def alphapat(n):
+
+	num = 65
+
+	for i in range(0, n):
+
+		for j in range(0, i+1):
+
+			ch = chr(num)
+
+			print(ch, end=" ")
+
+		num = num + 1
+
+		print("\r")
+
+n = 5
+alphapat(n)
