e9cf29ccdd239e61d008fd4ba970adb80d5d4e02
Shrey Kapri <124300646+shreykapri69@users.noreply.github.com>
2023-05-12 22:51:05 +0530

    Add files via upload

---
+++ Program to display the prime numbers within an interval.py
@@ -0,0 +1,14 @@
+# Program to display the prime numbers within an interval
+
+lower = 500
+upper = 700
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
