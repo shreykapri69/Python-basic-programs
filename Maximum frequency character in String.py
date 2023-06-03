556f8ed0bf2d8014229d5e50906bbddd93b4794a
Shrey Kapri <124300646+shreykapri69@users.noreply.github.com>
2023-06-03 22:48:55 +0530

    Add files via upload

---
+++ Maximum frequency character in String.py
@@ -0,0 +1,14 @@
+test_str = "GeeksforGeeks"
+
+print ("The original string is : " + test_str)
+
+
+all_freq = {}
+for i in test_str:
+if i in all_freq:
+all_freq[i] += 1
+else:
+all_freq[i] = 1
+res = max(all_freq, key = all_freq.get)
+
+print ("The maximum of all characters in GeeksforGeeks is : " + str(res))
